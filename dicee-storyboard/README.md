32fa67f0dadc93f1b4962edd8353f2132e66da39
diasGustaavo <diasgustaavo@icloud.com>
2022-12-23 17:50:17 -0300

    add swiftUI version

---
+++ dicee-storyboard/README.md
@@ -0,0 +1,5 @@
+# Dicee
+
+A simple random dices app made with swift storyboard.
+
+![Simulator Screen Shot - iPhone 14 Pro - 2022-12-04 at 15 23 16](https://user-images.githubusercontent.com/60455369/205508622-a4e5c179-4f6c-47c5-bc6d-f2a51ab4624f.png)
---
+++ dicee-swiftUI/dicee-swiftUI/dicee_swiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  dicee_swiftUIApp.swift
+//  dicee-swiftUI
+//
+//  Created by Gustavo Dias on 23/12/22.
+//
+
+import SwiftUI
+
+@main
+struct dicee_swiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
